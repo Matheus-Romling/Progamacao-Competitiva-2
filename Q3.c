dcafa661caf4f8845e56fb56489fca32c436ce94
Matheus Romling <matheus.romling.ra@gmail.com>
2026-06-27 20:49:53 -0300

    Implement main function to process input and output

---
+++ Q3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int n,k;
+        scanf("%d %d",&n,&k);
+        int impares=k/2;
+        if(k%2!=0 && n%2!=0){
+            impares++;
+        }
+        if(impares%2==0){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
